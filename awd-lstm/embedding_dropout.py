fa5808d31c2944fcdf3a520f7683da86a36c3a50
Victoria Pedlar <vpedlar@gmail.com>
2022-08-17 15:22:29 +0200

    Includes embedding dropout

---
+++ awd-lstm/embedding_dropout.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+
+def embedded_dropout(embed, words, dropout=0.1):
+    if dropout:
+        mask = embed.weight.data.new().resize_((embed.weight.size(0), 1)).bernoulli_(
+            1 - dropout
+        ).expand_as(embed.weight) / (1 - dropout)
+        masked_embed_weight = mask * embed.weight
+    else:
+        masked_embed_weight = embed.weight
+
+    padding_idx = embed.padding_idx
+    if padding_idx is None:
+        padding_idx = -1
+
+    X = torch.nn.functional.embedding(
+        words,
+        masked_embed_weight,
+        padding_idx,
+        embed.max_norm,
+        embed.norm_type,
+        embed.scale_grad_by_freq,
+        embed.sparse,
+    )
+    return X
