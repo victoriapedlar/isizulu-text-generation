526eae1a3ade390eedbca196854418884f49fcbf
Victoria Pedlar <vpedlar@gmail.com>
2022-08-18 11:02:30 +0200

    Creates datasets

---
+++ utils/get_autshumato.py
@@ -0,0 +1,52 @@
+import argparse
+import requests
+import zipfile
+import os
+from io import BytesIO
+import utils
+
+# take --output_dir command-line argument
+parser = argparse.ArgumentParser(description="Download Autshumato dataset (isiZulu).")
+parser.add_argument(
+    "--output_dir",
+    required=True,
+    default="data/autshumato",
+    help="directory where output files will be saved",
+)
+args = parser.parse_args()
+
+datasets = [
+    (
+        "https://master.dl.sourceforge.net/project/autshumato/Corpora/ENG-ZUL.Release.zip",  # url
+        "lcontent.DACB.DataVirVrystellingOpWeb.(eng-zul).zul.1.0.0.CAM.2010-09-23.txt",  # file_name
+        "isizulu/isizulu.txt",  # output_name
+        2606,  # lines_to_remove (constitution with poor formatting)
+    ),
+]
+
+for url, file_name, output_name, lines_to_remove in datasets:
+    print("processing:", url)
+    r = requests.get(url)
+    zip = zipfile.ZipFile(BytesIO(r.content))
+    corpus = zip.open(file_name)
+    corpus = corpus.read()
+    corpus = corpus.decode("utf-8")
+
+    sentences = corpus.splitlines()
+
+    sentences = utils.clean_sentences(
+        sentences, min_length=3, lines_to_remove=lines_to_remove
+    )
+
+    output_file_name = os.path.join(args.output_dir, output_name)
+    with open(output_file_name, "w", encoding="utf-8") as f:
+        f.write("\n".join(sentences))
+
+    print("total sentences in {}:".format(output_name), len(sentences))
+
+print(
+    "Autshumato datasets provided under Creative Commons Attribution Non-Commercial ShareAlike, "
+    "CTexT (Centre for Text Technology, North-West University), South Africa; "
+    "Department of Arts and Culture, South Africa. "
+    "http://autshumato.sourceforge.net/ and http://www.nwu.ac.za/ctext"
+)
---
+++ utils/getdata.sh
@@ -0,0 +1,31 @@
+echo "=== Acquiring datasets ==="
+echo "---"
+
+export PYTHONPATH=$PYTHONPATH:`pwd`/scripts
+
+echo "- Downloading NCHLT data"
+mkdir -p data/nchlt
+mkdir -p data/nchlt/isizulu
+python3 scripts/get_nchlt.py --output_dir=data/nchlt
+
+echo
+
+echo "- Downloading autshumato data"
+mkdir -p data/autshumato
+mkdir -p data/autshumato/isizulu
+python3 scripts/get_autshumato.py --output_dir=data/autshumato
+
+echo
+
+echo "- Downloading isolezwe data"
+mkdir -p data/isolezwe
+mkdir -p data/isolezwe/isizulu
+python3 scripts/get_isolezwe.py --output_dir=data/isolezwe
+
+echo
+
+echo "- Partitioning datasets"
+python3 scripts/partition_datasets.py
+
+echo "---"
+echo "Happy language modeling :)"
