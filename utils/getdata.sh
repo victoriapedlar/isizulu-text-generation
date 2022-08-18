bfb6a77859490fa134b9cd7c5a1b4fa1c5ca87e3
Victoria Pedlar <vpedlar@gmail.com>
2022-08-18 13:10:15 +0200

    Fetch data script added

--- utils/getdata.sh
+++ utils/getdata.sh
@@ -1,31 +1,31 @@
 echo "=== Acquiring datasets ==="
 echo "---"
 
-export PYTHONPATH=$PYTHONPATH:`pwd`/scripts
+export PYTHONPATH=$PYTHONPATH:`pwd`/utils
 
 echo "- Downloading NCHLT data"
 mkdir -p data/nchlt
 mkdir -p data/nchlt/isizulu
-python3 scripts/get_nchlt.py --output_dir=data/nchlt
+python3 utils/get_nchlt.py --output_dir=data/nchlt
 
 echo
 
 echo "- Downloading autshumato data"
 mkdir -p data/autshumato
 mkdir -p data/autshumato/isizulu
-python3 scripts/get_autshumato.py --output_dir=data/autshumato
+python3 utils/get_autshumato.py --output_dir=data/autshumato
 
 echo
 
 echo "- Downloading isolezwe data"
 mkdir -p data/isolezwe
 mkdir -p data/isolezwe/isizulu
-python3 scripts/get_isolezwe.py --output_dir=data/isolezwe
+python3 utils/get_isolezwe.py --output_dir=data/isolezwe
 
 echo
 
 echo "- Partitioning datasets"
-python3 scripts/partition_datasets.py
+python3 utils/partition_datasets.py
 
 echo "---"
 echo "Happy language modeling :)"
