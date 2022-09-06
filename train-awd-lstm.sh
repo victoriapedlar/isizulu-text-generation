78f7c311e036d47562387b724bf8d7dd80884672
Victoria Pedlar <vpedlar@gmail.com>
2022-09-06 10:19:07 +0200

    Add changes to bash script

--- train-awd-lstm.sh
+++ train-awd-lstm.sh
@@ -1,4 +1,21 @@
-pip install -qr requirements.txt
+#!/bin/sh
+
+#SBATCH --account=nlpgroup
+#SBATCH --partition=a100
+#SBATCH --nodes=1 --ntasks=2 --gres=gpu:a100-4g-20gb:1
+#SBATCH --time=48:00:00
+#SBATCH --job-name="AWD_LSTM_5_Sept"
+#SBATCH --mail-user=PDLVIC001@myuct.ac.za
+#SBATCH --mail-type=ALL
+
+CUDA_VISIBLE_DEVICES=$(ncvd)
+
+git clone https://github.com/victoriapedlar/isizulu-text-generation.git
+
+cd isizulu-text-generation
+
+# pip install -qr requirements.txt
+module load python/anaconda-python-3.7 
 
 ./utils/getdata.sh
 
