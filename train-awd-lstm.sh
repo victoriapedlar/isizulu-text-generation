e82fea56ee954d0853e43c35eada58d91556af22
Victoria Pedlar <vpedlar@gmail.com>
2022-09-20 14:15:25 +0200

    Big bash script train

--- train-awd-lstm.sh
+++ train-awd-lstm.sh
@@ -11,16 +11,15 @@ CUDA_VISIBLE_DEVICES=$(ncvd)
 
 module load python/anaconda-python-3.7
 module load software/TensorFlow-A100-GPU
-# source activate awd-lstm
 
 start=`date +%s`
 echo "Starting script..."
 
 python3 -u awd_lstm/main.py \
-    --save "awd-lstm-small.pt" \
-    --descriptive_name "AWDLSTM_Initial_Parameters" \
-    --data data/test \
-    --save_history "log_history.txt" \
+    --save "awd_lstm_combined.pt" \
+    --descriptive_name "awd_lstm_initial_parameters" \
+    --data data/combined/isizulu \
+    --save_history "awd_lstm_log_history.txt" \
     --emsize 800 \
     --nhid 1150 \
     --nlayers 3 \
@@ -38,7 +37,6 @@ python3 -u awd_lstm/main.py \
     --nonmono 8 \
     --cuda \
 
-# conda deactivate
 end=`date +%s`
 runtime=$(((end-start)/60))
 echo "Runtime with unspecified cores was $runtime minutes."
