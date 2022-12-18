c62249303c9213e6a3235e603975139531091242
Victoria Pedlar <vpedlar@gmail.com>
2022-12-18 13:01:27 +0200

    batch size tuning

--- train-awd-lstm.sh
+++ train-awd-lstm.sh
@@ -15,10 +15,11 @@ module load software/TensorFlow-A100-GPU
 start=$(date +%s)
 echo "Starting script..."
 
+echo "Batch size tuning"
 python3 -u awd_lstm/main.py \
     --descriptive_name "awd_lstm_combined" \
     --data data/combined/isizulu \
-    --save_history "logs/awd_lstm/${start}.txt" \
+    --save_history "logs/awd_lstm/$start.txt" \
     --emsize 800 \
     --nhid 1150 \
     --nlayers 3 \
@@ -26,6 +27,75 @@ python3 -u awd_lstm/main.py \
     --clip 0.25 \
     --epochs 750 \
     --batch_size 32 \
+    --bptt 70 \
+    --dropout 0.4 \
+    --dropouth 0.2 \
+    --dropouti 0.65 \
+    --dropoute 0.1 \
+    --wdrop 0.5 \
+    --seed 1882 \
+    --patience 4 \
+    --nonmono 8 \
+    --cuda \
+    --when 25 35 \
+
+python3 -u awd_lstm/main.py \
+    --descriptive_name "awd_lstm_combined" \
+    --data data/combined/isizulu \
+    --save_history "logs/awd_lstm/$start.txt" \
+    --emsize 800 \
+    --nhid 1150 \
+    --nlayers 3 \
+    --lr 30.0 \
+    --clip 0.25 \
+    --epochs 750 \
+    --batch_size 64 \
+    --bptt 70 \
+    --dropout 0.4 \
+    --dropouth 0.2 \
+    --dropouti 0.65 \
+    --dropoute 0.1 \
+    --wdrop 0.5 \
+    --seed 1882 \
+    --patience 4 \
+    --nonmono 8 \
+    --cuda \
+    --when 25 35 \
+
+python3 -u awd_lstm/main.py \
+    --descriptive_name "awd_lstm_combined" \
+    --data data/combined/isizulu \
+    --save_history "logs/awd_lstm/$start.txt" \
+    --emsize 800 \
+    --nhid 1150 \
+    --nlayers 3 \
+    --lr 30.0 \
+    --clip 0.25 \
+    --epochs 750 \
+    --batch_size 128 \
+    --bptt 70 \
+    --dropout 0.4 \
+    --dropouth 0.2 \
+    --dropouti 0.65 \
+    --dropoute 0.1 \
+    --wdrop 0.5 \
+    --seed 1882 \
+    --patience 4 \
+    --nonmono 8 \
+    --cuda \
+    --when 25 35 \
+
+python3 -u awd_lstm/main.py \
+    --descriptive_name "awd_lstm_combined" \
+    --data data/combined/isizulu \
+    --save_history "logs/awd_lstm/$start.txt" \
+    --emsize 800 \
+    --nhid 1150 \
+    --nlayers 3 \
+    --lr 30.0 \
+    --clip 0.25 \
+    --epochs 750 \
+    --batch_size 256 \
     --bptt 70 \
     --dropout 0.4 \
     --dropouth 0.2 \
