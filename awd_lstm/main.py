3c5bbe3c246bab19a497e4ccbb8f816fec79573c
Victoria Pedlar <vpedlar@gmail.com>
2022-12-20 21:15:22 +0200

    removes _ to fix TypeError

--- awd_lstm/main.py
+++ awd_lstm/main.py
@@ -571,7 +571,7 @@ try:
 
             # begin early stopping
             if epoch % args.eval_every == (args.eval_every - 1):
-                val_loss2, _ = evaluate(val_data)
+                val_loss2 = evaluate(val_data)
                 best_loss, stop_step, stop = early_stopping(
                     val_loss2, best_val_loss, stop_step, args.patience
                 )
