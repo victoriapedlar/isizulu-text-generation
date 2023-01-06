d47fa657ce1183f48d96e9a9cab5fecabb3453df
Victoria Pedlar <vpedlar@gmail.com>
2023-01-06 21:24:40 +0200

    10 epochs

--- train-sparse-lm.sh
+++ train-sparse-lm.sh
@@ -28,7 +28,7 @@ python3 -m sparse_text_generation.language_modeling.examples.run_lm_finetuning \
         --block_size 512 \
         --do_train \
         --evaluate_during_training \
-        --num_train_epochs 20 \
+        --num_train_epochs 10 \
         --loss entmax \
         --entmax_alpha 1.2 \
         --top_k 0 \
