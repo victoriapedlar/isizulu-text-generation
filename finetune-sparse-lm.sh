362739ba6d72ed4740692a12ce0eb121ea790b4d
Victoria Pedlar <vpedlar@gmail.com>
2023-01-07 19:27:34 +0200

    finetune sparse lm

---
+++ finetune-sparse-lm.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#SBATCH --account=nlpgroup
+#SBATCH --partition=a100
+#SBATCH --nodes=1 --ntasks=6 --gres=gpu:a100-3g-20gb:1
+#SBATCH --time=48:00:00
+#SBATCH --job-name="sparse_lm"
+#SBATCH --mail-user=PDLVIC001@myuct.ac.za
+#SBATCH --mail-type=ALL
+
+CUDA_VISIBLE_DEVICES=$(ncvd)
+
+module load python/anaconda-python-3.7
+module load software/TensorFlow-A100-GPU
+
+export LD_LIBRARY_PATH=/home/pdlvic001/.local/lib/python3.8/site-packages/nvidia/cublas/lib/:$LD_LIBRARY_PATH
+
+start=$(date +%s)
+echo "Starting script..."
+python3 -m sparse_text_generation.language_modeling.examples.run_lm_finetuning \
+        --train_data_file ~/isizulu-text-generation/data/isolezwe/isizulu/train.txt \
+        --eval_data_file ~/isizulu-text-generation/data/isolezwe/isizulu/valid.txt \
+        --output_dir ~/isizulu-text-generation/models/sparse_lm/finetune_7_Jan_22 \
+        --model_type gpt2 \
+        --model_name_or_path gpt2-medium \
+        --block_size 512 \
+        --do_train \
+        --evaluate_during_training \
+        --loss entmax \
+        --entmax_alpha 1.2 \
+        --top_k 0 \
+        --top_p 0
+
+end=$(date +%s)
+echo "Elapsed Time: $(($end-$start)) seconds"
