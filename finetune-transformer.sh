f4a6a8a371e907962fdd97335a1b3421a922b0d5
Victoria Pedlar <vpedlar@gmail.com>
2023-01-07 20:11:40 +0200

    finetune transformer

---
+++ finetune-transformer.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#SBATCH --account=nlpgroup
+#SBATCH --partition=a100
+#SBATCH --nodes=1 --ntasks=6 --gres=gpu:a100-4g-20gb:1
+#SBATCH --time=48:00:00
+#SBATCH --job-name="transformer"
+#SBATCH --mail-user=PDLVIC001@myuct.ac.za
+#SBATCH --mail-type=ALL
+
+CUDA_VISIBLE_DEVICES=$(ncvd)
+
+module load python/anaconda-python-3.7
+module load software/TensorFlow-A100-GPU
+
+start=$(date +%s)
+echo "Starting script..."
+
+export PYTHONPATH=$PYTHONPATH:`pwd`/scripts
+
+python3 scripts/finetune_example.py
+
+end=$(date +%s)
+echo "Elapsed Time: $(($end-$start)) seconds"
