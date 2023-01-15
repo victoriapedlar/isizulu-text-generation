65e910f2a8cb4b25e4afbbc54801cfe10f134d15
Victoria Pedlar <vpedlar@gmail.com>
2023-01-15 11:42:30 +0200

    updates

--- awd_lstm/main.py
+++ awd_lstm/main.py
@@ -425,49 +425,6 @@ def evaluate(data_source):
 # ------------- END ADJUSTED CODE --------------
 
 
-# def train():
-#     # Turn on training mode which enables dropout
-#     model.train()
-#     total_loss = 0.0
-#     start_time = time.time()
-#     ntokens = len(corpus.dictionary)
-#     hidden = model.init_hidden(args.batch_size)
-#     for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
-#         data, targets = get_batch(train_data, i, args)
-#         # Starting each batch, we detach the hidden state from how it was previously produced
-#         # If we didn't, the model would try backpropagating all the way to start of the dataset
-#         hidden = repackage_hidden(hidden)
-#         optimizer.zero_grad()
-#         output, hidden = model(data, hidden)
-
-#         loss = criterion(output.view(-1, ntokens), targets)
-#         loss.backward()
-
-#         # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs
-#         torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
-#         optimizer.step()
-
-#         total_loss += loss.item()
-
-#         if batch % args.log_interval == 0 and batch > 0:
-#             cur_loss = total_loss / args.log_interval
-#             elapsed = time.time() - start_time
-#             print(
-#                 "| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | "
-#                 "loss {:5.2f} | ppl {:8.2f}".format(
-#                     epoch,
-#                     batch,
-#                     len(train_data) // args.bptt,
-#                     lr,
-#                     elapsed * 1000 / args.log_interval,
-#                     cur_loss,
-#                     math.exp(cur_loss),
-#                 )
-#             )
-#             total_loss = 0
-#             start_time = time.time()
-
-
 def train(config=None):
     # Initialize a new wandb run
     with wandb.init(config=config):
@@ -660,23 +617,12 @@ try:
                 )
             )
             metrics = evaluate(val_data)
-            val_loss2, avg_perplexity, avg_jsd, avg_sp, bpc = metrics.values()
+            val_loss, avg_perplexity, avg_jsd, avg_sp, bpc = metrics.values()
             print(
                 "{} model params (SGD after eval)".format(
                     len([prm for prm in model.parameters()])
                 )
             )
-            # check if a variable is a numpy ndarray using the isinstance() function, convert it to a scalar if it is
-            if isinstance(val_loss, np.ndarray):
-                val_loss = val_loss.item()
-            if isinstance(avg_perplexity, np.ndarray):
-                avg_perplexity = avg_perplexity.item()
-            if isinstance(avg_jsd, np.ndarray):
-                avg_jsd = avg_jsd.item()
-            if isinstance(avg_sp, np.ndarray):
-                avg_sp = avg_sp.item()
-            if isinstance(bpc, np.ndarray):
-                bpc = bpc.item()
             print("-" * 89)
             print(
                 "| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | "
