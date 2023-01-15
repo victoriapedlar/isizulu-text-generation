3d9cb3f2ab9f8dc4b1f5569949d76d27365986f1
Victoria Pedlar <vpedlar@gmail.com>
2023-01-15 15:57:56 +0200

    updates

--- train-awd-lstm.sh
+++ train-awd-lstm.sh
@@ -17,8 +17,8 @@ start=$(date +%s)
 echo "Starting script..."
 
 python3 -u awd_lstm/main.py \
-    --descriptive_name "awd_lstm_test" \
-    --data data/test/ \
+    --descriptive_name "awd_lstm_combined" \
+    --data data/combined/isizulu/ \
     --save_history "logs/awd_lstm/$(date "+%Y-%m-%d_%H-%M-%S").txt" \
     --emsize 800 \
     --nhid 1150 \
@@ -26,14 +26,18 @@ python3 -u awd_lstm/main.py \
     --lr 30.0 \
     --clip 0.25 \
     --epochs 500 \
+    --batch_size 32 \
     --bptt 70 \
+    --dropout 0.2 \
     --dropouth 0.2 \
+    --dropouti 0.2 \
     --dropoute 0.1 \
+    --wdrop 0.2 \
     --seed 1882 \
+    --patience 3 \
     --nonmono 8 \
     --cuda \
-    --when 40 80 120 160 \
-
+    --when 40 80 120 \
 
 end=$(date +%s)
 echo "Elapsed Time: $(($end-$start)) seconds"
