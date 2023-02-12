d95964319ca0e1335498f63499fef21acf803a99
Victoria Pedlar <vpedlar@gmail.com>
2023-02-12 17:02:04 +0200

    removes tokenizer code

--- sparse_text_generation/language_modeling/examples/run_lm_finetuning.py
+++ sparse_text_generation/language_modeling/examples/run_lm_finetuning.py
@@ -1271,7 +1271,9 @@ def main():
             model.module if hasattr(model, "module") else model
         )  # Take care of distributed/parallel training
         model_to_save.save_pretrained(args.output_dir)
-        tokenizer.save_pretrained(args.output_dir)
+        # ------------------START CUSTOM CODE------------------#
+        # tokenizer.save_pretrained(args.output_dir)
+        # ------------------END CUSTOM CODE--------------------#
 
         # Good practice: save your training arguments together with the trained model
         torch.save(args, os.path.join(args.output_dir, "training_args.bin"))
@@ -1280,9 +1282,11 @@ def main():
         model = model_class.from_pretrained(
             args.output_dir, loss=loss_func, gen_func=gen_func
         )
-        tokenizer = tokenizer_class.from_pretrained(
-            args.output_dir, do_lower_case=args.do_lower_case
-        )
+        # ------------------START CUSTOM CODE------------------#
+        # tokenizer = tokenizer_class.from_pretrained(
+        #     args.output_dir, do_lower_case=args.do_lower_case
+        # )
+        # ------------------END CUSTOM CODE--------------------#
         model.to(args.device)
 
     # Evaluation
