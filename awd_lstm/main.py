d04ffa390e708f4d54f8851c5506caa20eca9763
Victoria Pedlar <vpedlar@gmail.com>
2023-02-15 21:55:18 +0200

    updates

--- awd_lstm/main.py
+++ awd_lstm/main.py
@@ -334,7 +334,8 @@ def evaluate(data_source, epsilon=0.000001, batch_size=10):
     hidden = model.init_hidden(batch_size)
     eval_dataloader = DataLoader(data_source, batch_size=batch_size)
     with torch.no_grad():
-        for i, (data, targets) in enumerate(eval_dataloader):
+        for i in range(0, data_source.size(0) - 1, args.bptt):
+            data, targets = get_batch(data_source, i, args)
             output, hidden = model(data, hidden)
             output_flat = output.view(-1, ntokens)
             total_loss += len(data) * criterion(output_flat, targets).item()
