7d1ef8ef53022b6631459f153959e389cf246284
Victoria Pedlar <vpedlar@gmail.com>
2023-02-26 20:55:44 +0200

    updates

--- scripts/gpt2_utils.py
+++ scripts/gpt2_utils.py
@@ -334,18 +334,18 @@ def evaluate(
         total_characters += len(test_set)
         encodings = tokenizers[language_id](test_set, return_tensors="pt")
 
-        # initialize log_probs tensor
-        batch_size = encodings.input_ids.shape[0]
-        seq_len = encodings.input_ids.shape[1]
-        log_probs = torch.zeros((batch_size, seq_len - 1))
-
         # adapted from https://huggingface.co/transformers/perplexity.html
         max_length = model.config.n_positions
         seq_len = encodings.input_ids.size(1)
 
+        nlls = []
+        prev_end_loc = 0
+
         for begin_loc in tqdm(range(0, seq_len, stride)):
             end_loc = min(begin_loc + max_length, seq_len)
-            trg_len = end_loc - begin_loc
+            trg_len = (
+                end_loc - prev_end_loc
+            )  # may be different from stride on last loop
             input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
             target_ids = input_ids.clone()
             target_ids[:, :-trg_len] = -100
@@ -353,24 +353,22 @@ def evaluate(
             with torch.no_grad():
                 outputs = model(input_ids, labels=target_ids)
 
-                # get the logits for the last token in each sequence
-                logits = outputs[1][..., :-1, :].contiguous()
-                logits = logits.view(-1, logits.size(-1))
+                # loss is calculated using CrossEntropyLoss which averages over input tokens.
+                # Multiply it with trg_len to get the summation instead of average.
+                # We will take average over all the tokens to get the true average
+                # in the last step of this example.
+                neg_log_likelihood = outputs[0] * trg_len
 
-                # apply softmax to get the probabilities
-                probs = torch.softmax(logits, dim=1)
-                if len(probs[0].nonzero()) != len(probs[0]):
-                    probs = probs[:, :] + epsilon
-                    sums = [probs[i].sum().item() for i in range(probs.size(0))]
-                    probs = [probs[i] / sums[i] for i in range(len(sums))]
-                    probs = torch.stack(probs)
+                neg_log_likelihood += epsilon
 
-                # calculate log probabilities and add to tensor
-                log_probs[:, begin_loc : end_loc - 1] = torch.log(probs + epsilon)
+            nlls.append(neg_log_likelihood)
 
-        # calculate perplexity
-        avg_log_prob = torch.mean(log_probs)
-        ppl = torch.exp(-1.0 / avg_log_prob) / ((1 + epsilon) ** (seq_len - 1))
+            prev_end_loc = end_loc
+            if end_loc == seq_len:
+                break
+
+        nlls = torch.cat(nlls, dim=1)
+        ppl = torch.exp(torch.mean(nlls))
 
         sp = 0
         jsd = 0
