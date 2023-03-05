592faf82a54af9d12c695d2503a02de9f5fd91a9
Victoria Pedlar <vpedlar@gmail.com>
2023-03-05 19:53:23 +0200

    updates lr

--- train-sparse-lm.sh
+++ train-sparse-lm.sh
@@ -28,7 +28,7 @@ python3 -m sparse_text_generation.language_modeling.examples.run_lm_finetuning \
         --block_size 512 \
         --do_train \
         --evaluate_during_training \
-        --learning_rate 1e-4 \
+        --learning_rate 0.001 \
         --loss entmax \
         --entmax_alpha 1.2 \
         --top_k 0 \
